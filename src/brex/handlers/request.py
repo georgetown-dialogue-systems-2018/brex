22017b09382acb700e1c09ec191659ab9138d39d
Luke Gessler <ldg3fa@virginia.edu>
2018-12-09 21:28:19 -0500

    flesh out request

--- src/brex/handlers/request.py
+++ src/brex/handlers/request.py
@@ -1,8 +1,11 @@
+import logging
+
 from brex.handlers.handler import Handler
 from brex.template_renderer import TemplateRenderer
 
 
-requestable_entities = ['title', 'author', 'review']
+requestable_entities = ['title', 'author', 'summary', 'rating']
+
 class Request(Handler):
     def __init__(self):
         self._renderer = TemplateRenderer('request')
@@ -17,7 +20,9 @@ class Request(Handler):
         elif entity == 'author':
             return {'author': book.authors}
         elif entity == 'summary':
-            return {'description': book.description}
+            return {'summary': book.description}
+        elif entity == 'rating':
+            return {'rating': book.average_rating}
         else:
             raise Exception('Tried to request an unsupported entity: {}'.format(entity))
 
@@ -26,7 +31,27 @@ class Request(Handler):
         return self._renderer.render('book', {'book': book})
 
     def _generate_text(self, context, wit_response, system_intent):
-        return str(system_intent)
+        if 'failure' in system_intent:
+            reason = system_intent['failure']
+            if reason == 'no_current_book':
+                return self._renderer.render('no_current_book')
+            else:
+                raise Exception('Tried to generate text for unknown failure "{}"'.format(reason))
+        elif 'title' in system_intent:
+            title = system_intent['title']
+            return self._renderer.render('title', {'title': title})
+        elif 'author' in system_intent:
+            authors = system_intent['author']
+            author = authors[0]
+            return self._renderer.render('author', {'author': author})
+        elif 'summary' in system_intent:
+            summary = system_intent['summary']
+            return self._renderer.render('summary', {'summary': summary})
+        elif 'rating' in system_intent:
+            rating = system_intent['rating']
+            return self._renderer.render('rating', {'rating': rating})
+        else:
+            raise Exception('Tried to generate text for some unknown entity. System intent: {}'.format(system_intent))
 
     def handle(self, context, wit_response):
         output = {}
@@ -35,13 +60,16 @@ class Request(Handler):
         system_intent = {}
         for entity, value in entities.items():
             if entity in requestable_entities:
-                result = self._fetch_entity(self, context, wit_response, entity)
+                result = self._fetch_entity(context, wit_response, entity)
+                logging.debug("Fetched entity: " + str(result))
                 if 'failure' in result:
                     system_intent = result
                     break
                 else:
                     # i.e., merge the `result` dict into `system_intent`
                     system_intent.update(result)
+
+        logging.debug("System intent: {}".format(system_intent))
 
         output['text'] = self._generate_text(context, wit_response, system_intent)
 
